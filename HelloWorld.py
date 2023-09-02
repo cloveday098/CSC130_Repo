0f1768b7990297539fb458a9cc7d9a0d69c91e46
Loveday, Chance <lovedaycb@ornl.gov>
2023-09-02 13:26:46 -0400

    Cleaned up files

--- HelloWorld.py
+++
@@ -1,7 +0,0 @@
-#print("Welcome to Python\nHello World!")
-#name = input("Well hello there! Who are you? ")
-#print("Hello " + name)
-#print("Hello", name)
-
-price = float(input("How much was dinner? "))
-print("The tip is", price*0.2, "The total is", price*1.2)
